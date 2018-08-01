c4d8e461d1da7a0fcc2127553133426d599a4d59
Jim Zhan <jim@EPCNSZXW0354s-Mac-mini.local>
2018-08-01 19:12:19 +0800

    fix(jest): CLI to `execa` to receive proper exit status

--- tests/helpers.spec.js
+++ tests/helpers.spec.js
@@ -3,6 +3,6 @@ const helpers = require('../tasks/helpers')
 describe(helpers.pkg.name, () => {
   it('getPackageSettings()', () => {
     const url = helpers.getPackageSettings('repository')
-    expect(url).toBe('s://github.com/jimzhan/esnext-scripts')
+    expect(url).toBe('https://github.com/jimzhan/esnext-scripts')
   })
 })
