02961778904e944355bc06436c53a08d0751cc43
Jim Zhan <jim@EPCNSZXW0354s-Mac-mini.local>
2018-08-08 10:32:12 +0800

    chore(cli): enable `ora` progress reminder

--- commands/new.js
+++ commands/new.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const ora = require('ora')
 const plop = require('node-plop')(
   path.resolve(__dirname, 'generators', 'plopfile.js'),
 )
@@ -13,7 +14,9 @@ exports.handler = (argv) => {
   const { name } = argv
   const app = plop.getGenerator('new')
 
+  const spinner = ora('Start creating new workspace').start()
   app.runActions({ name }).then(() => {
-
+    // @TODO results based display.
+    spinner.succeed(`workspace <${name}> has been created`)
   })
 }
