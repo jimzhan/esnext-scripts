db843e1c65101e553deea5eb6bbba27b94150ccc
Jim Zhan <jim.zhan@me.com>
2018-08-09 14:20:33 +0800

    chore(jest): bump up `jest` timeout to 1 minute

--- etc/jest.enzyme.js
+++ etc/jest.enzyme.js
@@ -1,4 +1,7 @@
+const jest = require('jest-cli')
 const Enzyme = require('enzyme')
 const Adapter = require('enzyme-adapter-react-16')
+
+jest.setTimeout(60000)
 
 Enzyme.configure({ adapter: new Adapter() })
