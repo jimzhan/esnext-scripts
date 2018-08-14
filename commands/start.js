50cf25a244820186e9211e00db1b555b49a0c2a8
Jim Zhan <jim_zhan@epam.com>
2018-08-15 00:47:35 +0800

    feat(start): enable skeleton for `start`

--- commands/start.js
+++ commands/start.js
@@ -1,38 +1,61 @@
-const { CLIEngine } = require('eslint')
+const os = require('os')
+const pm2 = require('pm2')
 const helpers = require('./helpers')
 
 
+
+exports.command = 'start <script>'
+exports.desc = 'Start the application script'
+
 /**
- * Perform linting via `eslint` API.
- * @param {String} dir - directory to perform linting.
+ * Start a development process with ESM supports for the given application.
+ * @param {String} script - application entry to start with.
  * @param {Object} argv - `yargs` options.
  */
-const lintFromApi = (dir, argv) => { // eslint-disable-line
-  const options = require('../.eslintrc') // eslint-disable-line
-  const eslint = new CLIEngine(Object.assign({
-    fix: !!argv.fix, // @FIXME doesn't really work.
-    extensions: ['.js', '.jsx'],
-  }, options))
-  const { log } = console
-  const { results } = eslint.executeOnFiles([dir])
-  const formatter = eslint.getFormatter('stylish')
-  const output = formatter(results)
-  if (output) log(output)
-  helpers.info(`--fix ${argv.fix}`)
+const startForDevelopment = (script, argv) => { // eslint-disable-line
+  const args = [
+    script,
+    '-r', 'esm',
+  ]
+  helpers.execute('node', args)
 }
 
 /**
- * Execute `eslint` from CLI.
- * @param {String} dir - directory to perform linting.
+ * Starting the given app in cluster mode that will leverage all CPUs available .
+ * @param {String} app - applicaton entry to start with.
+ * @param {Object} argv - `yargs` options.
+
+ */
+const startForProduction = (script, argv) => { // eslint-disable-line
+  const cpus = os.cpus().length
+
+  pm2.connect((err) => {
+    if (err) {
+      helpers.error(err)
+      process.exit(2)
+    }
+    pm2.start({
+      script,
+      exec_mode: 'cluster',
+      instances: cpus,
+      max_memory_restart: '100M',
+      node_args: '-r esm',
+    }, (error) => {
+      pm2.disconnect()
+      if (error) throw error
+    })
+  })
+}
+
+/**
+ * Execute `eslint` linting with default settings under current `cwd`..
  * @param {Object} argv - `yargs` options.
  */
-const lintFromCli = (dir, argv) => { // eslint-disable-line
-  const args = [
-    dir,
-    '--ext', '.js,jsx',
-    '--config', require.resolve('../.eslintrc'),
-  ]
-  if (argv.fix) args.push('--fix')
-  if (argv.fixDryRun) args.push('--fix-dry-run')
-  helpers.execute(require.resolve('eslint/bin/eslint'), args)
+exports.handler = (argv) => {
+  const { script } = argv
+  if (process.env.NODE_ENV === 'production') {
+    startForProduction(script, argv)
+  } else {
+    startForDevelopment(script, argv)
+  }
 }
