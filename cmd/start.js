0c60badfe3710eae00c8e0641d775d7a5755ee00
Jim Zhan <jim.zhan@me.com>
2019-05-21 23:47:29 +0800

    feat: WIP adding pm2-runtime supports

--- cmd/start.js
+++ cmd/start.js
@@ -1,4 +1,6 @@
+const os = require('os')
 const path = require('path')
+
 const { consts, helpers } = require('../lib')
 
 exports.command = 'start <script>'
@@ -36,9 +38,14 @@ const startForDevelopment = async (script, argv) => {
  * @param {String} app - applicaton entry to start with.
  * @param {Object} argv - `yargs` options.
  */
-const startForProduction = async (config, argv) => {
+const startForProduction = async (script, argv) => {
   const { cmd } = consts
-  const args = [ 'start', path.resolve(config) ]
+  const instances = argv.instances || os.cpus().length
+
+  const args = [
+    'start', path.resolve(script),
+    `--instances=${instances}`
+  ]
   helpers.execute(cmd.pm2, args)
 }
 
@@ -57,3 +64,21 @@ exports.handler = (argv) => {
     startForDevelopment(script, argv)
   }
 }
+
+/*
+  -i --instances <number>    launch [number] of processes automatically load-balanced. Increase overall performances and performance stability.
+  --secret [key]             [MONITORING] keymetrics secret key
+  --public [key]             [MONITORING] keymetrics public key
+  --machine-name [name]      [MONITORING] keymetrics machine name
+  --raw                      raw log output
+  --json                     output logs in json format
+  --format                   output logs formated like key=val
+  --delay <seconds>          delay start of configuration file by <seconds>
+  --web [port]               launch process web api on [port] (default to 9615)
+  --only <application-name>  only act on one application of configuration
+  --no-auto-exit             do not exit if all processes are errored/stopped or 0 apps launched
+  --env [name]               inject env_[name] env variables in process config file
+  --watch                    watch and restart application on file change
+  --error <path>             error log file destination (default disabled)
+  --output <path>            output log file destination (default disabled)
+*/
