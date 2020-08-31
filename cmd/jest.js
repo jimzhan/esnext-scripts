eae1099b2d3d4b76ee678a3331a0dc04d93772b7
jimzhan <jim.zhan@me.com>
2020-08-31 11:17:14 +0800

    feat: enable optional test folder supports for `jest`

--- cmd/jest.js
+++ cmd/jest.js
@@ -8,17 +8,24 @@ const filename = 'jest.config.js';
 
 exports.command = 'test';
 exports.desc = 'run `jest` test specs';
+exports.builder = (yargs) =>
+  yargs.option('dir', {
+    alias: 'd',
+    type: 'string',
+    description: 'directory to start testing (default: cwd)',
+  });
 
 /**
  * Format scripts under given glob pattern.
  * @param {Object} argv - `yargs` options.
  * @TODO - custom options.
  */
-exports.handler = async () => {
+exports.handler = async (argv) => {
+  const dir = argv.dir || consts.cwd;
   const options = await core.readSettings({ filename, namespace });
   const config = merge(options, settings);
 
-  const { results } = await jest.runCLI(config, [consts.cwd]);
+  const { results } = await jest.runCLI(config, [dir]);
 
   process.exit(results.success ? 0 : 1);
 };
